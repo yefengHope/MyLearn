0c07ac49e32f15bbc9f75a61be3bad623386dd96
hanfengHope <1825445185@qq.com>
2018-01-06 15:13:58 +0800

    【spring boot】- 【多环境配置】

---
+++ SpringBoot/multi-envir/src/main/java/com/example/MultiEnvirApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiEnvirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiEnvirApplication.class, args);
+	}
+}
